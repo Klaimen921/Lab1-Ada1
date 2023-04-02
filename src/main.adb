9659aacf95b5d5a93905684c1e4b93956cec0dea
kachanov mykola <76394135+Klaimen921@users.noreply.github.com>
2023-04-02 19:55:03 +0300

    Ada

---
+++ src/main.adb
@@ -0,0 +1,56 @@
+with Ada.Text_IO;
+with Ada.Integer_Text_IO;
+procedure Main is
+
+
+
+
+
+   task type break_thread is
+
+   end break_thread;
+   task type main_thread is
+      entry Set (Steps : Long_Long_Integer; Ids : Integer);
+   end main_thread;
+
+
+      time : Duration;
+      id :Integer;
+   begin
+
+            time := times;
+            id := Ids;
+   end Set;
+
+
+   end break_thread;
+
+
+      step : Long_Long_Integer;
+      col  : Long_Long_Integer := 0;
+      sum  : Long_Long_Integer := 0;
+      id : Integer;
+   begin
+
+            step := Steps;
+            id := ids;
+   end Set;
+
+
+         sum := sum + col * step;
+         exit when can_stop(id);
+      end loop;
+
+   end main_thread;
+
+
+
+
+begin
+
+      M (I).Set(N (I),I);
+      S (I).Set(S_D(I),I);
+
+   end loop;
+
+end Main;
